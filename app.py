06aad183466eea9cacad4373f4b16f4b448d0feb
Juan Antonio <JuantAldea@users.noreply.github.com>
2022-06-17 12:35:17 +0200

    Autoformat

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ from flask import Flask, render_template, request, redirect
 from PIL import Image
 from torchvision import models
 
+with open("imagenet_classes.txt") as f:
+    imagenet_class_index = [line.strip() for line in f.readlines()]
+
+model = models.googlenet(pretrained=True)
+model.eval()
+
+app = Flask(__name__)
+
+
 def classify(image_bytes):
     try:
         tensor = transform_image(image_bytes=image_bytes)
@@ -17,18 +26,19 @@ def classify(image_bytes):
 
     return max_prob_index, imagenet_class_index[max_prob_index]
 
+
 def transform_image(image_bytes):
     preprocess = transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                             0.229, 0.224, 0.225]),
     ])
 
     image = Image.open(io.BytesIO(image_bytes))
     return preprocess(image).unsqueeze(0)
 
-app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -46,17 +56,11 @@ def upload_file():
 
         image_string = base64.b64encode(img_bytes).decode()
 
-        class_id, class_name = classify(image_bytes = img_bytes)
+        class_id, class_name = classify(image_bytes=img_bytes)
 
-        return render_template('result.html', class_id=class_id, class_name=class_name, image_mime_type = file.content_type, image_base64 = image_string)
+        return render_template('result.html', class_id=class_id, class_name=class_name, image_mime_type=file.content_type, image_base64=image_string)
     return render_template('index.html')
 
-
-with open("imagenet_classes.txt") as f:
-    imagenet_class_index = [line.strip() for line in f.readlines()]
-
-model = models.googlenet(pretrained=True)
-model.eval()
 
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
